1b514097d0e8560b5c32dfaa43b7707fa24f0448
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-15 23:21:44 -0400

    confirmed function of API query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
-const searchEl = document.querySelector('#search');
+const cityEl = document.querySelector('.form-input');
+const enterEl = document.querySelector('#search');
 const APIKey = "bc4b56299d21aec4bb34ccdca89fbde5";
-let city = "atlanta";
+let city = 'atlanta';
 
 const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
 
 function getCityWeatherData() {
     fetch(queryURL).then(response => response.json()).then(response => {
+        debugger;
+        console.log(response);
     });
-    console.log(response);
-    return;
+console.log("true")
+event.preventDefault()
 }
 
-searchEl.addEventListener('click', getCityWeatherData);
+enterEl.addEventListener('click', getCityWeatherData);
