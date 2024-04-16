bb19081d065f64ba4c294ad2b79ed716c983c3ee
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-15 21:21:48 -0400

    updated layout styling for html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+const searchEl = document.querySelector('#search');
 const APIKey = "bc4b56299d21aec4bb34ccdca89fbde5";
+let city = "atlanta";
+
+const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+
+function getCityWeatherData() {
+    fetch(queryURL).then(response => response.json()).then(response => {
+    });
+    console.log(response);
+    return;
+}
+
+searchEl.addEventListener('click', getCityWeatherData);
